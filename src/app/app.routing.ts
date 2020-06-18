10157c70db91752c62e38b3cd376805899897a8f
Tomás <tomas.sa467@gmail.com>
2020-06-18 04:01:33 -0400

    feat: optimize code and compress img in webp

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,24 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule,} from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {Routes, RouterModule} from '@angular/router';
 
-import { PresentationComponent } from './presentation/presentation.component';
+import {PresentationComponent} from './presentation/presentation.component';
 
-const routes: Routes =[
-    { path: 'presentation',         component: PresentationComponent },
-    { path: '', redirectTo: 'presentation', pathMatch: 'full' }
+const routes: Routes = [
+  {path: '', component: PresentationComponent},
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
-    imports: [
-        CommonModule,
-        BrowserModule,
-        RouterModule.forRoot(routes,{
-          useHash: true
-        })
-    ],
-    exports: [
-    ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+      useHash: true
+    })
+  ],
+  exports: [],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/presentation/presentation.component.ts
+++ src/app/presentation/presentation.component.ts
@@ -1,9 +1,6 @@
-import {Component, OnInit, OnDestroy, HostListener, ElementRef} from '@angular/core';
-import {ServerService} from "../service/server.service";
-import {ServerResponse} from "../types/request";
-
-declare const $: any;
-
+import {Component, OnInit, OnDestroy, HostListener} from '@angular/core';
+import {ServerService} from '../service/server.service';
+import {ServerResponse} from '../types/request';
 
 @Component({
   selector: 'app-presentation',
@@ -13,17 +10,27 @@ declare const $: any;
 
 export class PresentationComponent implements OnInit, OnDestroy {
   serverData: ServerResponse;
-  myParams: object = {};
-  myStyle: object = {};
-  model = {
-    left: true,
-    middle: false,
-    right: false
-  };
   date: Date = new Date();
 
+  myParams = {
+    particles: {
+      number: {
+        value: 50,
+      },
+      color: {
+        value: '#fff'
+      }
+    }
+  };
+
+  myStyle = {
+    'z-index': 10,
+    height: '100%',
+    width: '100%',
+    position: 'absolute'
+  };
+
   constructor(
-    public el: ElementRef,
     private serverService: ServerService
   ) {
   }
@@ -33,8 +40,8 @@ export class PresentationComponent implements OnInit, OnDestroy {
     const componentPosition = document.getElementsByClassName('add-animation');
     const scrollPosition = window.pageYOffset;
 
-    for (var i = 0; i < componentPosition.length; i++) {
-      var rec = componentPosition[i].getBoundingClientRect().top + window.scrollY + 100;
+    for (let i = 0; i < componentPosition.length; i++) {
+      const rec = componentPosition[i].getBoundingClientRect().top + window.scrollY + 100;
       if (scrollPosition + window.innerHeight >= rec) {
         componentPosition[i].classList.add('animated');
       } else if (scrollPosition + window.innerHeight * 0.8 < rec) {
@@ -44,26 +51,10 @@ export class PresentationComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.myParams = {
-      particles: {
-        number: {
-          value: 50,
-        },
-        color: {
-          value: '#fff'
-        }
-      }
-    };
-    this.myStyle = {
-      'z-index': 10,
-      height: '100%',
-      width: '100%',
-      position: 'absolute'
-    };
-    var body = document.getElementsByTagName('body')[0];
+    const body = document.getElementsByTagName('body')[0];
     body.classList.add('presentation-page');
     body.classList.add('loading');
-    var navbar = document.getElementsByTagName('nav')[0];
+    const navbar = document.getElementsByTagName('nav')[0];
     navbar.classList.add('navbar-transparent');
 
     this.serverService.getServerData().toPromise().then((data: ServerResponse) => {
@@ -72,10 +63,10 @@ export class PresentationComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    var body = document.getElementsByTagName('body')[0];
+    const body = document.getElementsByTagName('body')[0];
     body.classList.remove('presentation-page');
     body.classList.remove('loading');
-    var navbar = document.getElementsByTagName('nav')[0];
+    const navbar = document.getElementsByTagName('nav')[0];
     navbar.classList.remove('navbar-transparent');
   }
 }
